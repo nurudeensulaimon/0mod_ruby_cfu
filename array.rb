6a784e27563060451ba67866be878dab870a443b
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-06-21 15:26:52 -0400

    Use methods on array.

--- array.rb
+++ array.rb
@@ -2,6 +2,12 @@ friends =["Mikky","Mouse","Tom","Jerry", "Nero"]
 
 income =[20,40,60,80,200]
 
-receipts =[12.5, 25.5 , 27.3, 30.2 69.5]
+receipt =[12.5, 25.5, 27.3, 30.2,69.5]
 
 attendance = [true, true, false, false, true]
+
+# Call each methon on the array
+puts friends.pop
+puts income.push 
+puts receipt.shift 
+puts attendance.unshift
