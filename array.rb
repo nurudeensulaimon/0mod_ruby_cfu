52a8e4063d49b477841d926f7620727663c80845
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-06-21 16:15:25 -0400

    Add a new array method

--- array.rb
+++ array.rb
@@ -12,13 +12,14 @@ attendance = [true, true, false, false, true]
 puts friends.pop
 puts income.push 
 puts receipt.shift 
-puts attendance.unshift
+puts attendance.unshift(true)
 
 # friends.pop removes the last variable in the friends array 
 # income.push prints the entire array starting from index[0] to the last
 # index 
 #receipt.shift prints out index[0] only 
-# attendance.unshift prints out the entire array starting from index[0]
+# attendance.unshift will add a new element to the array.
+
 
 =Demonstrate Understand of index positions 
 Index can be used to extract variable from the array. For example,
@@ -28,6 +29,19 @@ We can also select more than one element from our arrays. For exmaple
 friends[0,3] will select from zeroth element to the second element. Same strategy 
 applies for selecting from income, receipt, and attendance.
 =end 
+
+# Add insert array method 
+put friends.insert("samson")
+put income.insert(400)
+put receipt.insert(99.9)
+put attendance.insert(false)
+
+= This array methos is used to insert new element 
+into our already created array. We can also use this 
+method to insert different data types into the array.
+=end 
+
+
 
 
 
