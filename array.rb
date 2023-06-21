b69afbfea226e41b4deac1d99ee75dd03b347461
Nurudeen Sulaimon <nerodean@Neros-MacBook-Pro.local>
2023-06-21 15:54:15 -0400

    Explain each method

--- array.rb
+++ array.rb
@@ -6,8 +6,19 @@ receipt =[12.5, 25.5, 27.3, 30.2,69.5]
 
 attendance = [true, true, false, false, true]
 
-# Call each methon on the array
+# Call each method on the array
+
+
 puts friends.pop
 puts income.push 
 puts receipt.shift 
 puts attendance.unshift
+
+# friends.pop removes the last variable in the friends array 
+# income.push prints the entire array starting from index[0] to the last
+# index 
+#receipt.shift prints out index[0] only 
+# attendance.unshift prints out the entire array starting from index[0]
+
+
+
